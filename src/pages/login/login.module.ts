60efd853f650732a5b86490cee590b4f341b1ab8
Kenny Chancafe <kendu.script@gmail.com>
2017-06-24 02:41:23 -0300

    importando modulos compartidos

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,13 +1,15 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
 @NgModule({
   declarations: [
-    LoginPage,
+    LoginPage
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
+    SharedModule
   ],
   exports: [
     LoginPage
