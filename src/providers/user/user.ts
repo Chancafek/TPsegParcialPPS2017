b13cff9bb713424ce44e61ac0e4c0c70ebe52b79
Kenny Chancafe <kendu.script@gmail.com>
2017-06-24 02:40:41 -0300

    service de usuarios

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,18 +1,52 @@
+import { User } from './../../models/user';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the UserProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class UserProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello UserProvider Provider');
-  }
+  private url = 'http://educadroid.dev'
+
+  constructor(public http: Http) { }
+
+  getUsers(): Observable<User[]> {
+        return this.http.get(`${this.url}/users`, this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    getUserByID(id: any): Observable<User> {
+        return this.http.get(`${this.url}/users/${id}`, this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    editUser(id: any, body: any): Observable<User> {
+        return this.http.put(`${this.url}/users/${id}`, body,this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    addUser(body: any): Observable<any> {
+        return this.http.post(`${this.url}/users`, body, this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    removeUser(id: any): Observable<any> {
+        return this.http.delete(`${this.url}/users/${id}`, this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    private jwt() {
+        const currentUser = JSON.parse(localStorage.getItem('token_argenta'));
+        if (currentUser) {
+            const headers = new Headers({ 'Authorization': 'Bearer ' + currentUser });
+            return new RequestOptions({ headers: headers });
+        }
+    }
 
 }
