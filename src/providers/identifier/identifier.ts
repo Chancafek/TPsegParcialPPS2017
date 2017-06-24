d69442a3f1443d047baed4926f1aa9e8c23e7baa
Kenny Chancafe <kendu.script@gmail.com>
2017-06-24 15:26:20 -0300

    service identificador

---
+++ src/providers/identifier/identifier.ts
@@ -0,0 +1,53 @@
+import { User } from './../../models/user';
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { JwtHelper } from 'angular2-jwt';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class IdentityProvider {
+
+    jwtHelper: JwtHelper;
+
+    private url = 'http://educadroid.dev';
+
+    constructor(private http: Http) {
+      this.jwtHelper = new JwtHelper();
+    }
+
+    getIdentity() {
+        const token = localStorage.getItem('token_educadroid');
+        if(token) {
+          const decodedToken = this.jwtHelper.decodeToken(token);
+          let subject = {
+              id : decodedToken.sub,
+              nombre: decodedToken.user.nombre,
+              apellido: decodedToken.user.apellido,
+              email : decodedToken.user.email,
+              role : decodedToken.role,
+          };
+          return subject;
+        } else {
+          console.log('error token no encontrado');
+        }
+    }
+
+    getUserProfile(id?: any): Observable<User> {
+        if (id == null || id === '') {
+            id = this.getIdentity().id;
+        }
+        return this.http.get(`${this.url}/users/${id}`, this.jwt())
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().message || 'Server Error'));
+    }
+
+    private jwt() {
+        const currentUser = JSON.parse(localStorage.getItem('token_educadroid'));
+        if (currentUser) {
+            const headers = new Headers({ 'Authorization': 'Bearer ' + currentUser });
+            return new RequestOptions({ headers: headers });
+        }
+    }
+}
