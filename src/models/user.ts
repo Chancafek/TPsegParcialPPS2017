cb12c51fdf017342eb46abfd43ad97f8ad5844be
Kenny Chancafe <kendu.script@gmail.com>
2017-06-25 16:41:04 -0300

    fix atributos

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,20 +4,20 @@ import { Domicilio } from "../models/domicilio";
 export class User {
 
   public id?: Number;
-  public nombre: String;
-  public apellido: String;
-  public documento: String;
-  public legajo: String;
-  public email: String;
-  public password: String;
-  public fnacimiento: Date;
-  public rol: ERol;
-  public image: String;
-  public sexo: String;
-  public domicilio: Domicilio;
+  public nombre?: String;
+  public apellido?: String;
+  public documento?: String;
+  public legajo?: String;
+  public email?: String;
+  public password?: String;
+  public fnacimiento?: Date;
+  public rol?: ERol;
+  public image?: String;
+  public sexo?: String;
+  public domicilio?: Domicilio;
 
   constructor() {
 
   }
-  
+
 }
