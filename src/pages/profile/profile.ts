cdcc5301480c49404a5db66b101ff3b1f3a931a2
Kenny Chancafe <kendu.script@gmail.com>
2017-06-27 20:48:49 -0300

    mostrar info de usuario y cambio de imagen desde la camara o galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,10 @@
+import { User } from './../../models/user';
+import { IdentityProvider } from './../../providers/identifier/identifier';
+import { UserProvider } from './../../providers/user/user';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the ProfilePage page.
@@ -14,11 +19,92 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  perfil: any;
+  private cameraOptions: CameraOptions;
+  private galleryOptions: CameraOptions;
+  public profileImage: string = 'assets/img/male.png';
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _userService: UserProvider,
+    private identifier: IdentityProvider,
+    private actionSheetCtrl: ActionSheetController,
+    private camera: Camera
+    ) {
+      // this.identifier.getUserProfile()
+      //   .subscribe(
+      //     data => this.perfil = data,
+      //     err => console.log(err)
+      //   );
+
+      this.cameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        targetHeight: 512,
+        targetWidth: 512,
+        allowEdit: true,
+        correctOrientation: true,
+      }
+      this.galleryOptions = {
+        quality: 100,
+        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        targetHeight: 512,
+        targetWidth: 512,
+        allowEdit: true,
+        correctOrientation: true,
+      }
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+
+  }
+
+  cambiarFoto() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Cambiar imagen de perfil',
+      cssClass: 'profile-img-actionsheet',
+      buttons: [
+        {
+          text: 'Galeria',
+          icon: 'md-images',
+          handler: () => {
+            this.camera.getPicture(this.galleryOptions).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            this.profileImage = imageData;
+            }, (err) => {
+             console.log(err);
+            });
+          }
+        },{
+          text: 'Foto',
+          icon: 'md-camera',
+          handler: () => {
+            this.camera.getPicture(this.cameraOptions).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            this.profileImage = imageData;
+            }, (err) => {
+             console.log(err);
+            });
+          }
+        },{
+          text: 'Cancel',
+          icon: 'md-redo',
+          role: 'cancel',
+        }
+      ],
+      enableBackdropDismiss: true
+    });
+    actionSheet.present();
   }
 
 }
