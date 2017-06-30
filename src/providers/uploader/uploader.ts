61b53fbef43d4ef4338bc6680527a05ed7f190dc
Kenny Chancafe <kendu.script@gmail.com>
2017-06-30 10:44:57 -0300

    uploader service

---
+++ src/providers/uploader/uploader.ts
@@ -0,0 +1,52 @@
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the UploaderProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UploaderProvider {
+
+  private userUrl = 'http://educadroid/users/'
+
+  constructor(
+    private transfer: Transfer
+  ) {
+    console.log('Hello UploaderProvider Provider');
+  }
+
+  uploadImage(id: any, img: any) {
+    const fileTransfer: TransferObject = this.transfer.create();
+    const currentUser = JSON.parse(localStorage.getItem('token_educadroid'));
+      let options: FileUploadOptions = {
+        fileKey: 'file',
+        fileName: 'name.jpg',
+        headers: {
+          'Authorization': 'Bearer '+ currentUser
+        }
+
+      }
+
+      fileTransfer.upload(img, `${this.userUrl}/${id}/upload`, options)
+      .then((data) => {
+        console.log('success');
+      }, (err) => {
+        console.log('error al subir')
+      })
+    }
+
+  private jwt() {
+        const currentUser = JSON.parse(localStorage.getItem('token_educadroid'));
+        if (currentUser) {
+            const headers = new Headers({ 'Authorization': 'Bearer ' + currentUser });
+            return new RequestOptions({ headers: headers });
+        }
+    }
+
+
+}
